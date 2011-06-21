5c424db076eee0ef6d9d2cab783f9d93fa5159eb
renctan <renctan@gmail.com>
2011-06-21 10:42:57 -0400

    added more tests for SolrSynchronizer

--- test/integration/solr_synchronizer_test.rb
+++ test/integration/solr_synchronizer_test.rb
@@ -32,15 +32,82 @@ class SolrSynchronizerTest < Test::Unit::TestCase
       solr.sync { return }
     end
 
-    should "update db contents to solr after dumping" do
+    should "update db insertions to solr after dumping" do
+      solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE)
       @solr.expects(:add).at_most_once
       @solr.expects(:commit).times(2)
-      solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE)
 
       solr.sync do |mode, doc_count|
         if mode == :finished_dumping then
           @test_coll1.insert({ "msg" => "Hello world!" })
         elsif mode == :sync then
+          return
+        end
+      end
+    end
+
+    should "update multiple db insertions to solr after dumping" do
+      solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE)
+      @solr.expects(:add).twice
+      @solr.expects(:commit).times(2..3)
+
+      solr.sync do |mode, doc_count|
+        if mode == :finished_dumping then
+          @test_coll1.insert({ "msg" => "Hello world!" })
+          @test_coll2.insert({ "author" => "Matz" })
+        elsif mode == :sync and doc_count >= 2 then
+          return
+        end
+      end
+    end
+
+    should "update db updates to solr after dumping" do
+      @test_coll1.insert({ "msg" => "Hello world!" })
+
+      solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE)
+      @solr.expects(:add).twice
+      @solr.expects(:commit).times(2)
+
+      solr.sync do |mode, doc_count|
+        if mode == :finished_dumping then
+          @test_coll1.update({ "msg" => "Hello world!" }, {"$set" => {"from" => "Tim Berners"}})
+        elsif mode == :sync then
+          return
+        end
+      end
+    end
+
+    should "update deleted db contents to solr after dumping" do
+      @test_coll1.insert({ "msg" => "Hello world!" })
+
+      solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE)
+      @solr.stubs(:add)
+      @solr.expects(:delete_by_id).once
+      @solr.expects(:commit).at_least(2)
+
+      solr.sync do |mode, doc_count|
+        if mode == :finished_dumping then
+          @test_coll1.remove({ "msg" => "Hello world!" })
+        elsif mode == :sync then
+          return
+        end
+      end
+    end
+
+    should "update db inserts, updates and deletes to solr after dumping" do
+      @test_coll1.insert({ "msg" => "Hello world!" })
+
+      solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE)
+      @solr.expects(:add).at_most(3)
+      @solr.expects(:delete_by_id).once
+      @solr.expects(:commit).times(2..4)
+
+      solr.sync do |mode, doc_count|
+        if mode == :finished_dumping then
+          @test_coll1.update({ "msg" => "Hello world!" }, {"$set" => {"from" => "Tim Berners"}})
+          @test_coll1.remove({ "msg" => "Hello world!" })
+          @test_coll1.insert({ "lang" => "Ruby" })
+        elsif mode == :sync and doc_count >= 3 then
           return
         end
       end
