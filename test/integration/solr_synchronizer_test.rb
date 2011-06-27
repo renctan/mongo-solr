d3940fe2785bacb08910559c2b31e73e9463cdb2
renctan <renctan@gmail.com>
2011-06-27 13:05:10 -0400

    Added more tests for selective filtering

--- test/integration/solr_synchronizer_test.rb
+++ test/integration/solr_synchronizer_test.rb
@@ -143,89 +143,136 @@ class SolrSynchronizerTest < Test::Unit::TestCase
         @db_set_coll1[@db.name] = db_coll
       end
 
-      should "perform update on collection in the list (single db)" do
-        solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
-        solr.logger = DEFAULT_LOGGER
+      context "pre-defined collection set" do
+        should "perform update on collection in the list (single db)" do
+          solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
+          solr.logger = DEFAULT_LOGGER
 
-        @solr.expects(:add).once
-        @solr.expects(:commit).twice
+          @solr.expects(:add).once
+          @solr.expects(:commit).twice
 
-        solr.sync do |mode, doc_count|
-          if mode == :finished_dumping then
-            @test_coll1.insert({ "lang" => "Ruby" })
-          elsif mode == :sync then
-            break
+          solr.sync do |mode, doc_count|
+            if mode == :finished_dumping then
+              @test_coll1.insert({ "lang" => "Ruby" })
+            elsif mode == :sync then
+              break
+            end
+          end
+        end
+
+        should "perform update on collection in the list (2 db)" do
+          db2_coll = MongoSolr::SynchronizedSet.new
+          db2_coll.add(@test_coll3.name)
+          @db_set_coll1[@db2.name] = db2_coll
+          solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
+          solr.logger = DEFAULT_LOGGER
+
+          @solr.expects(:add).twice
+          @solr.expects(:commit).times(2..3)
+
+          solr.sync do |mode, doc_count|
+            if mode == :finished_dumping then
+              @test_coll1.insert({ "lang" => "Ruby" })
+              @test_coll3.insert({ "lang" => "Ruby" })
+            elsif mode == :sync then
+              break
+            end
+          end
+        end
+
+        should "perform update on collection in the list (same db, diff coll)" do
+          @db_set_coll1[@db.name].add(@test_coll2.name)
+
+          solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
+          solr.logger = DEFAULT_LOGGER
+
+          @solr.expects(:add).twice
+          @solr.expects(:commit).times(2..3)
+
+          solr.sync do |mode, doc_count|
+            if mode == :finished_dumping then
+              @test_coll1.insert({ "lang" => "Ruby" })
+              @test_coll2.insert({ "auth" => "Matz" })
+            elsif mode == :sync then
+              break
+            end
+          end
+        end
+
+        should "not perform update on collection not in the list (different db)" do
+          solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
+          solr.logger = DEFAULT_LOGGER
+
+          @solr.expects(:add).never
+          @solr.stubs(:commit)
+
+          solr.sync do |mode, doc_count|
+            if mode == :finished_dumping then
+              @test_coll3.insert({ "lang" => "Ruby" })
+            elsif mode == :sync then
+              break
+            end
+          end
+        end
+
+        should "not perform update on collection not in the list (same db, diff coll)" do
+          solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
+          solr.logger = DEFAULT_LOGGER
+
+          @solr.expects(:add).never
+          @solr.stubs(:commit)
+
+          solr.sync do |mode, doc_count|
+            if mode == :finished_dumping then
+              @test_coll2.insert({ "lang" => "Ruby" })
+            elsif mode == :sync then
+              break
+            end
           end
         end
       end
 
-      should "perform update on collection in the list (2 db)" do
-        db2_coll = MongoSolr::SynchronizedSet.new
-        db2_coll.add(@test_coll3.name)
-        @db_set_coll1[@db2.name] = db2_coll
-        solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
-        solr.logger = DEFAULT_LOGGER
+      context "dynamic collection set modification" do
+        should "perform update after being added in the list (single db)" do
+          db_set = MongoSolr::SynchronizedHash.new
+          solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, db_set)
+          solr.logger = DEFAULT_LOGGER
 
-        @solr.expects(:add).twice
-        @solr.expects(:commit).times(2..3)
+          @solr.expects(:add).once
+          @solr.stubs(:commit)
 
-        solr.sync do |mode, doc_count|
-          if mode == :finished_dumping then
-            @test_coll1.insert({ "lang" => "Ruby" })
-            @test_coll3.insert({ "lang" => "Ruby" })
-          elsif mode == :sync then
-            break
+          solr.sync do |mode, doc_count|
+            if mode == :finished_dumping then
+              @test_coll1.insert({ "lang" => "Ruby" })
+            elsif mode == :sync and doc_count == 0 then
+              coll_set = MongoSolr::SynchronizedSet.new
+              coll_set.add(@test_coll1.name)
+              db_set[TEST_DB] = coll_set
+
+              @test_coll1.insert({ "auth" => "Matz" })
+            else
+              break
+            end
           end
         end
-      end
 
-      should "perform update on collection in the list (same db, diff coll)" do
-        @db_set_coll1[@db.name].add(@test_coll2.name)
+        should "not perform update after being deleted from the list (single db)" do
+          db_set = MongoSolr::SynchronizedHash.new
+          solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
+          solr.logger = DEFAULT_LOGGER
 
-        solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
-        solr.logger = DEFAULT_LOGGER
+          @solr.expects(:add).once
+          @solr.stubs(:commit)
 
-        @solr.expects(:add).twice
-        @solr.expects(:commit).times(2..3)
-
-        solr.sync do |mode, doc_count|
-          if mode == :finished_dumping then
-            @test_coll1.insert({ "lang" => "Ruby" })
-            @test_coll2.insert({ "auth" => "Matz" })
-          elsif mode == :sync then
-            break
-          end
-        end
-      end
-
-      should "not perform update on collection not in the list (different db)" do
-        solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
-        solr.logger = DEFAULT_LOGGER
-
-        @solr.expects(:add).never
-        @solr.stubs(:commit)
-
-        solr.sync do |mode, doc_count|
-          if mode == :finished_dumping then
-            @test_coll3.insert({ "lang" => "Ruby" })
-          elsif mode == :sync then
-            break
-          end
-        end
-      end
-
-      should "not perform update on collection not in the list (same db, diff coll)" do
-        solr = MongoSolr::SolrSynchronizer.new(@solr, @connection, MODE, @db_set_coll1)
-        solr.logger = DEFAULT_LOGGER
-
-        @solr.expects(:add).never
-        @solr.stubs(:commit)
-
-        solr.sync do |mode, doc_count|
-          if mode == :finished_dumping then
-            @test_coll2.insert({ "lang" => "Ruby" })
-          elsif mode == :sync then
-            break
+          solr.sync do |mode, doc_count|
+            if mode == :finished_dumping then
+              @test_coll1.insert({ "lang" => "Ruby" })
+            elsif mode == :sync and doc_count == 0 then
+              db_set[TEST_DB].delete(@test_coll1.name)
+              @test_coll1.insert({ "auth" => "Matz" })
+            else
+              break
+            end
           end
         end
       end
