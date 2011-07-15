cafb135f1df3f89665dd157964782bc8fbaffd91
renctan <renctan@gmail.com>
2011-07-15 17:28:34 -0400

    Missed file from previous commit

---
+++ src/solr_config_const.rb
@@ -0,0 +1,10 @@
+module MongoSolr
+  # Class that contains the constants used for the Solr Config Database.
+  class SolrConfigConst
+    MONGO_SOLR_COLLECTION_NAME = "mongo_solr"
+    SOLR_URL_KEY = "url"
+    NS_KEY = "ns"
+    COLL_FIELD_KEY = "f"
+  end
+end
+
