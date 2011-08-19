840ecae8923caa0959fbf79b3f2cb5d1e70914d8
renctan <renctan@gmail.com>
2011-08-19 10:29:52 -0400

    Added message when loading the shell plugin using the bash script

--- mongo_solr.sh
+++ mongo_solr.sh
@@ -3,5 +3,6 @@
 # A very simple script to start the Mongo shell preloaded with the plugin.
 # This script assumes that the Mongo shell is in the executable path.
 
+echo "Loading the MongoSolr extensions..."
 mongo $* --eval "load('solr-plugin.js')" --shell
 
